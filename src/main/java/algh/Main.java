b5fb64026d94c2184fc163fe4b76987d8ea369b8
Natan <natanhenriqueramossilva@gmail.com>
2026-05-04 22:15:17 -0300

    feat: algoritomo insertion sort

--- src/main/java/algh/Main.java
+++ src/main/java/algh/Main.java
@@ -1,6 +1,7 @@
 package algh;
 
 
+import algh.datastructureandalghinjava.algoritmos.InsertionSort;
 import algh.datastructureandalghinjava.poo.ArithProgression;
 import algh.datastructureandalghinjava.poo.FiboProgression;
 import algh.datastructureandalghinjava.poo.GeomProgression;
@@ -9,18 +10,23 @@ import algh.datastructureandalghinjava.poo.Progression;
 public class Main {
     public static void main(String[] args) {
 
-        Progression prog = new Progression();
-        ArithProgression arith = new ArithProgression();
-        GeomProgression geom = new GeomProgression();
-        FiboProgression fibo = new FiboProgression();
+//        Progression prog = new Progression();
+//        ArithProgression arith = new ArithProgression();
+//        GeomProgression geom = new GeomProgression();
+//        FiboProgression fibo = new FiboProgression();
+//
+//        arith.printProgression(30);
+//
+//        prog.printProgression(30);
+//
+//        fibo.printProgression(30);
+//
+//        geom.printProgression(30);
 
-        arith.printProgression(30);
+        char[] a = {'a', 'b', 'd', 'c'};
 
-        prog.printProgression(30);
-
-        fibo.printProgression(30);
-
-        geom.printProgression(30);
-
+        InsertionSort sort = new InsertionSort();
+        System.out.println(sort.insertionSort(a));
+        sort.insertionSort(a);
     }
 }
---
+++ src/main/java/algh/datastructureandalghinjava/algoritmos/InsertionSort.java
@@ -0,0 +1,16 @@
+package algh.datastructureandalghinjava.algoritmos;
+
+public class InsertionSort {
+
+    public char[] insertionSort(char[] a){
+        int n = a.length;
+        for (int i = 1; i< n; i++){
+            char cur = a[i];
+            int j = i - 1;
+            while ((j >= 0) && (a[j] > cur))
+                a[j + 1] = a[j--];
+            a[j + 1] = cur;
+        }
+    return a;
+    }
+}
